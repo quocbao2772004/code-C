0a620429daaefadd88d8b7e0cc0715664e5c470a
quocbao2772004 <123377752+quocbao2772004@users.noreply.github.com>
2023-01-26 20:15:33 +0700

    Update C01040.c

--- C01040.c
+++ C01040.c
@@ -3,24 +3,28 @@
 long long n;
 long long m;
 long long  a,b,c;
+
 long long tong;
+
 long long arr1[10001];
 int arr2[10001];
-
+int kt(long long n)
+{
+    int t=1;
+    for(int i=2;i<=sqrt(n);i++)
+        if(n%i==0)
+    {
+        if(i*i==n)t=t+i;
+        else t=t+i+n/i;
+    }
+    if(t==n)return 1;
+    return 0;
+}
 int main()
 {
     scanf("%lld",&n);
 
-    int luu1=n%10;
-    int m=n;
-    int d=0;
-    int luu2;
-    while(m>0)
-    {
-        d++;
 
-        m/=10;
-    }
-   printf("%d",d);
+   printf("%d",kt(n));
     return 0;
 }
