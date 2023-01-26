56c84f46bb0af454aa5dbb11491b9cf3f69398fa
quocbao2772004 <123377752+quocbao2772004@users.noreply.github.com>
2023-01-26 20:16:20 +0700

    Create C01039.c

---
+++ C01039.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+long long n;
+long long m;
+long long  a,b,c;
+
+long long tong;
+
+long long arr1[10001];
+int arr2[10001];
+
+int main()
+{
+    scanf("%lld",&n);
+
+    int luu1=n%10;
+    int m=n;
+    int d=0;
+    int luu2;
+    while(m>0)
+    {
+        d++;
+
+        m/=10;
+    }
+   printf("%d",d);
+    return 0;
+}
